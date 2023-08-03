0d1597cf30e918b76e4ee50a8799254db799b0bb
Long-Codegym <longvtv2003@gmail.com>
2023-08-03 15:08:46 +0700

    11

--- backend/src/main/java/com/repository/ICommentReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.model.CommentReply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICommentReplyRepository extends JpaRepository<CommentReply,Long> {
-}
