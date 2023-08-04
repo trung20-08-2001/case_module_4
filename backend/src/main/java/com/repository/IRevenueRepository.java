6fe7e7ddf0d0137b7d0f0e022a0124c1eda34593
Long-Codegym <longvtv2003@gmail.com>
2023-08-05 05:13:04 +0700

    up revenue

---
+++ backend/src/main/java/com/repository/IRevenueRepository.java
@@ -0,0 +1,16 @@
+package com.repository;
+import com.model.Revenue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface IRevenueRepository extends JpaRepository<Revenue,Long> {
+
+    @Query(value = "SELECT r FROM Revenue r WHERE r.month = :month AND r.year = :year ")
+    List<Revenue> getAllRevenueByMonthAndYear(@Param("month") int month, @Param("year") int year);
+
+    @Query(value ="SELECT r FROM Revenue r WHERE r.year = :year " )
+    List<Revenue> getAllRevenueByYear(@Param("year") int year);
+}
